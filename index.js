6d9472072fdebbafdc7e9eda02752a3226131e1b
yasmine-touati <yasmine.touati@etudiant-isi.utm.tn>
2025-06-14 00:11:02 +0100

    i could make the page more fun

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.static('public'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`App running at http://localhost:${PORT}`);
+  console.log(`App is running on port ${PORT}`);
 });
